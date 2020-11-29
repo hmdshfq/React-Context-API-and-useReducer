538c164e4816d5402ffbcd39e0c1bc7a7455b025
Hammad Shafiq <hmdshfq@outlook.com>
2020-11-29 11:36:09 +0100

    Added Reducer

--- src/Parent.js
+++ src/Parent.js
@@ -1,10 +1,12 @@
 import Child from "./Child";
+import ChildTwo from "./ChildTwo";
 
 function Parent() {
   return (
     <div>
       <h2>Parent</h2>
       <Child />
+      <ChildTwo />
     </div>
   );
 }
